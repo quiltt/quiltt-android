1c5c888b978742c4eac85e67108af5cbd0b3988d
Tom Lee <tom@quiltt.io>
2023-12-30 16:00:53 -0800

    Example for testing against mavenLocal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,5 +45,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation(project(":connector"))
+    implementation("app.quiltt:quiltt-connector:0.0.0")
 }
--- connector/build.gradle.kts
+++ connector/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
     namespace = "app.quiltt.connector"
-    compileSdk = 33
+    compileSdk = 26
 
     defaultConfig {
         minSdk = 26
@@ -34,10 +35,28 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("quiltt-connector") {
+                groupId = "app.quiltt"
+                artifactId = "quiltt-connector"
+                version = "0.0.0"
+
+                artifact("$buildDir/outputs/aar/connector-release.aar")
+            } // app.quiltt:quiltt-connector:0.0.0
+        }
+        repositories {
+            maven {
+                name = "quiltt-connector-kotlin"
+                url = uri("${project.buildDir}/quiltt-connector-kotlin")
+            }
+        }
+    }
 }
