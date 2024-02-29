63469ca5b22e1431b5925fca12dd9029e09be706
Tom Lee <tom@quiltt.io>
2024-02-28 21:08:17 -0800

    More work on Android Quiltthub Webview app
    Still need to solve below problem:
    1. Upon logout and log back in, the hub webapp doesn't get the token
    2. Haven't handle token expires

--- app_jetpack_compose/src/main/java/app/quiltt/app_jetpack_compose/AppConfig.kt
+++
@@ -1,7 +0,0 @@
-package app.quiltt.app_jetpack_compose
-
-object AppConfig {
-    val ingressConnectorId = BuildConfig.INGRESS_CONNECTOR_ID
-    val addConnectorId = BuildConfig.ADD_CONNECTOR_ID
-    val oauthRedirectUrl = "https://quiltt.app/mobile/hub/reconnect"
-}
