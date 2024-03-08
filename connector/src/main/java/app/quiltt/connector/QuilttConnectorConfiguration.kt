bb8f9064341057f8237bb128a2e7b7e8b743fbd4
tom <108757230+tom-quiltt@users.noreply.github.com>
2024-03-08 06:09:50 -0800

    Expose Institutions option in Android SDK (#11)
    * Expose Institutions option in Android SDK
    
    * institution is optional, connectionId is required in reconnect

--- connector/src/main/java/app/quiltt/connector/QuilttConnectorConfiguration.kt
+++ connector/src/main/java/app/quiltt/connector/QuilttConnectorConfiguration.kt
@@ -4,11 +4,13 @@ interface QuilttConnectorConfiguration {
     val connectorId: String
     val oauthRedirectUrl: String
     val connectionId: String?
+    val institution: String?
 }
 
 data class QuilttConnectorConnectConfiguration(
     override val connectorId: String,
     override val oauthRedirectUrl: String,
+    override val institution: String? = null,
 ) : QuilttConnectorConfiguration {
     override val connectionId: String? = null // always null for connect, cannot be set
 }
@@ -16,5 +18,7 @@ data class QuilttConnectorConnectConfiguration(
 data class QuilttConnectorReconnectConfiguration(
     override val connectorId: String,
     override val oauthRedirectUrl: String,
-    override var connectionId: String?
-) : QuilttConnectorConfiguration
+    override val connectionId: String,
+) : QuilttConnectorConfiguration {
+    override val institution: String? = null // always null for reconnect, cannot be set
+}
