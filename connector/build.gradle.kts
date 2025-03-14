92e9f0b12f55d9c9aeb01f716374d3031b05139c
zubairaziz <zubairaziz.dev@gmail.com>
2025-03-15 06:22:01 +0800

    Bump version

--- connector/build.gradle.kts
+++ connector/build.gradle.kts
@@ -46,7 +46,7 @@ publishing {
         register<MavenPublication>("connector") {
             groupId = "app.quiltt"
             artifactId = "connector"
-            version = "0.0.6"
+            version = "0.0.7"
 
             pom {
                 name.set("Quiltt Connector")
